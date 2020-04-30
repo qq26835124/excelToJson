9f4a4b2ee60b5dc0bdca00cec87148253b839c4a
jiangyichun <jiangyichun@jiangyichundeMacBook-Pro.local>
2020-04-30 17:01:46 +0800

    第一次提交

---
+++ tools/README.md
@@ -0,0 +1,11 @@
+# 使用说明
+
+> 命令行执行如下命令：
+
+```sh
+  npm run excelToJson [source] [targetName] [path]
+  # source 是将要转换成json的excel文件，必选参数
+  # targetName 转换成功后的json文件名，可选（默认名为output.json）
+  # path 转换成功后的json文件所在目录，可选（默认当前目录）
+  # eg. npm run excelToJson example.xls example
+```
---
+++ tools/excelToJson.js
@@ -0,0 +1,44 @@
+const xl = require('xlsx');
+const fs = require('fs');
+const path = require('path');
+
+let args = process.argv.splice(2);
+
+console.log(args)
+
+if (!args[0]) return;
+
+let input, output, dir;
+
+if (args[0] && (args[0].endsWith('.xls') || args[0].endsWith('.xlsx'))) {
+  input = args[0];
+  output = args[1] && args[1] + '.json' || 'output.json';
+  dir = args[2] || path.resolve(__dirname, '../');
+} else {
+  console.log('只能转换excel格式文件!');
+}
+
+//workbook 对象，指的是整份 Excel 文档。我们在使用 js-xlsx 读取 Excel 文档之后就会获得 workbook 对象。
+const workbook = xl.readFile(input)
+
+const sheetNames = workbook.SheetNames;
+
+const worksheet = workbook.Sheets[sheetNames[0]];
+
+//返回json数据
+let data = xl.utils.sheet_to_json(worksheet);
+
+console.log('data:', data);
+
+if (output) {
+  let dataStr = JSON.stringify(data);
+  fs.writeFile(dir + '/' + output, dataStr, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('转换成功!');
+    }
+  })
+}
+
+// console.log('TODO 将转换后的数据显示到浏览器页面上!')
